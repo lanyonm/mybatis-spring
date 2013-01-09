72c3e508281a76ebbef10475bec7f0e61c5e94a9
eduardo <eduardo.macarron@gmail.com>
2013-01-09 23:33:22 +0100

    namespace handler and schema moved to config package

--- src/main/java/org/mybatis/spring/config/NamespaceHandler.java
+++ src/main/java/org/mybatis/spring/config/NamespaceHandler.java
@@ -13,8 +13,9 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package org.mybatis.spring.mapper;
+package org.mybatis.spring.config;
 
+import org.mybatis.spring.mapper.MapperScannerBeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
 /**
